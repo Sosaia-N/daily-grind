de60537e1ad41b497140722a5dc901723f10d2cc
Sosaia Ngauamo <Sngauamo@sea.year.org>
2024-11-20 15:19:34 -0800

    added bubble tea

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -1,5 +1,30 @@
-myDate = new Date();
-myDay = myDate.getDay();
+/*
+
+    Rotate by Day of Week: Use HTML/CSS & JavaScript to create a single page that rotates unique colors and content for each weekday (Sunday to Saturday) into the page.  The content must include:
+
+    One unique image, with appropriate and matching content in the alt tag.  
+    A paragraph or two of content that describes the daily item (paragraph must include the name of the highlighted weekday)
+    A unique color that supports the image and paragraph of content
+    The unique color could affect the background HTML, or an element on the page for each day of the week.  All of the above must occur within one page.
+
+    name - for exmple Bubble Tea
+    pic - for exmple bubble tea.jpg
+    alt - name of coffee in alt tag
+    color - color associated with coffee
+    desc - description of coffee
+    day - day of the week
+
+*/
+
+
+
+
+
+
+let myDate = new Date();
+let myDay = myDate.getDay();
+let coffee = "";
+let today = "";
 
 switch(myDay){
 
@@ -11,6 +36,20 @@ switch(myDay){
        today = "Tuesday";
    break;
 
+   case 3:
+    today = "wednesday";
+
+      coffee = {
+        name: "Bubble Tea",
+        pic: "images/bubble-tea.jpg",
+        alt: "a pic of bubble tea",
+        color: "pink",
+        day: "wednesday",
+        desc: ' I like me some bubblr tea!'
+      }; 
+
+break;
+
    default:
       today = "Something went wrong";
 
@@ -18,3 +57,15 @@ switch(myDay){
 }
 
 alert(today);
+console.log(coffee);
+
+document.getElementById("coffee-cup").innerHTML = coffeetemplate(coffee);
+
+
+
+function coffeetemplate(coffee){
+    return `        <p>
+    <img src="${coffee.pic}" alt="${coffee.alt}" id="coffee" />
+   <strong class="feature">${coffee.day}'s Coffee Special:</strong> ${coffee.day}'s daily coffee special is <strong class="feature">${coffee.name}</strong>, ${coffee.desc}</p>`;
+}
+
